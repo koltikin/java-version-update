f35f619478ce8c53d0743f79d1d837c468f54a29
ziya <koltikin210@gmail.com>
2023-07-10 06:27:08 +0300

    five class was added

--- m1-generics/src/main/java/com/cydeo/Main.java
+++ m1-generics/src/main/java/com/cydeo/Main.java
@@ -1,7 +1,25 @@
 package com.cydeo;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        ArrayList<Integer> items = new ArrayList<Integer>();
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        items.add(5);
+        
+        printDoubled(items);
+
+
+    }
+
+    private static void printDoubled(ArrayList<Integer> items) {
+        for (int item : items) {
+            System.out.println(item*2);
+        }
+
     }
 }
