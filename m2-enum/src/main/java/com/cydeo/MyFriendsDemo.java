5d47e17c2c29de021cf0597b20adf806588c9b28
ziya <koltikin210@gmail.com>
2023-07-11 20:21:15 +0300

    enum is done

---
+++ m2-enum/src/main/java/com/cydeo/MyFriendsDemo.java
@@ -0,0 +1,9 @@
+package com.cydeo;
+
+public class MyFriendsDemo {
+    public static final MyFriendsDemo ZIYA = new MyFriendsDemo();
+    public static final MyFriendsDemo MUHAMMED = new MyFriendsDemo();
+    public static final MyFriendsDemo YUSUF = new MyFriendsDemo();
+    public static final MyFriendsDemo ERKIN = new MyFriendsDemo();
+
+}
