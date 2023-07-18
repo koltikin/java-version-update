7f24ddfaa03efcac868ce6d2d710926c1e152a24
ziya <koltikin210@gmail.com>
2023-07-18 14:57:38 +0300

    Stream intro is done

---
+++ m6-Stream/src/main/java/com/cydeo/Main.java
@@ -0,0 +1,32 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //        creating Stream from Array
+        String[] courses = {"Java","Spring","Agile"};
+        Stream<String> courseStream = Arrays.stream(courses);
+
+        //        creating Stream from Collections
+        List<String> courseList = Arrays.asList("Java","Spring","Agile");
+        Stream<String> courseListStream = courseList.stream();
+
+        List<Course> myCourses = Arrays.asList(
+                new Course("Java",18),
+                new Course("Python",19),
+                new Course("C++",20),
+                new Course("C#",21)
+        );
+
+        Stream<Course> myCoursesStream = myCourses.stream();
+
+
+
+
+
+    }
+}
